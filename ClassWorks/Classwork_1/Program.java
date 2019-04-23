25c5f10e4be3f9397baef7a6a3d6e0dcd38e2a7a
Oleg Igonin <oleg_in_l@mail.ru>
2019-04-23 19:54:31 +0300

    add ClassWork_1 started

---
+++ ClassWorks/Classwork_1/Program.java
@@ -0,0 +1,9 @@
+int number = 135
+a = number % 2 // 64
+b = (number/2) % 2 // 32
+c = (number/4) % 2 // 16
+d = (number/6) % 2 // 8
+e = (number/8) % 2 // 4
+f = (number/10) % 2 // 2
+g = (number/12) % 2 // 1
+System.out.println()
