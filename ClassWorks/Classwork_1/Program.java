84ac8527368f134d8f2ee62a394c1b3f3193b4c6
Oleg Igonin <oleg_in_l@mail.ru>
2019-04-23 23:34:00 +0300

    add code

--- ClassWorks/Classwork_1/Program.java
+++ ClassWorks/Classwork_1/Program.java
@@ -1,9 +1,15 @@
-int number = 135
-a = number % 2 // 64
-b = (number/2) % 2 // 32
-c = (number/4) % 2 // 16
-d = (number/6) % 2 // 8
-e = (number/8) % 2 // 4
-f = (number/10) % 2 // 2
-g = (number/12) % 2 // 1
+class Program {
+	public static void main(String[] args) {
+		int number = 135
+	int a = number % 2 // 64
+	int b = (number/2) % 2 // 32
+	int c = (number/4) % 2 // 16
+	int d = (number/6) % 2 // 8
+	int e = (number/8) % 2 // 4
+	int f = (number/10) % 2 // 2
+	int g = (number/12) % 2 // 1
+	int h = (number/14) % 2 //
+	}
+
 System.out.println()
+}
