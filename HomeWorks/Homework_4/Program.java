1851d7766132065663e2c9ba77921315d1d7cee0
Oleg Igonin <oleg_in_l@mail.ru>
2019-05-01 19:24:19 +0300

    application change Homework_4

--- HomeWorks/Homework_4/Program.java
+++ HomeWorks/Homework_4/Program.java
@@ -1,74 +1,30 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 class Program {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int chislo = 0;
-		int digits1 = 0;
-		int digits2 = 0;
-		int digits3 = 0;
-		int digits4 = 0;
-		int digits5 = 0;
-		int digits6 = 0;
-		int digits7 = 0;
-		int digits8 = 0;
-		int digits9 = 0;
-		List <Integer> numbers = new ArrayList <>();
-		
-			while (chislo != -1) {
-			chislo = scanner.nextInt();
-			numbers.add(chislo);
-			int filter = 0;
-
-			while (chislo > 0) {
-				filter = chislo%10;
-				chislo = chislo/10;
-
-				if (filter == 1) {
-					digits1++;
+		int arrray[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+		int filter = 0;
+		int i;
+		int number = 0;
+		while (true) {
+			if (number == -1) {
+				break;
+			}
+			number = scanner.nextInt();
+			while (number > 0) {
+				filter = number % 10;
+				number = number / 10;
+			for (i = 1; i < 11; i++) {
+				if (filter == i) {
+					arrray[i - 1] = arrray[i - 1] + 1;
 				}
-					if (filter == 2) {
-						digits2++;
-					}
-						if (filter == 3) {
-							digits3++;
-						}
-							if (filter == 4) {
-								digits4++;
-							}
-								if (filter == 5) {
-									digits5++;
-								}
-									if (filter == 6) {
-										digits6++;
-									}
-										if (filter == 7) {
-											digits7++;
-										}
-											if (filter == 8) {
-												digits8++;
-											}
-												if (filter == 9) {
-													digits9++;
-												}
-
-		} 
-				if (chislo == -1) {
-					digits1++;
-					break;
+			}		
 			}
 		}
-		System.out.println("Kolichestvo 1 = " + digits1);
-		System.out.println("Kolichestvo 2 = " + digits2);
-		System.out.println("Kolichestvo 3 = " + digits3);
-		System.out.println("Kolichestvo 4 = " + digits4);
-		System.out.println("Kolichestvo 5 = " + digits5);
-		System.out.println("Kolichestvo 6 = " + digits6);
-		System.out.println("Kolichestvo 7 = " + digits7);
-		System.out.println("Kolichestvo 8 = " + digits8);
-		System.out.println("Kolichestvo 9 = " + digits9);
+		for (i = 1;i <= 9; i++) {
+			System.out.println("Number of digits " + i + " = " + arrray[i - 1]);
+		}
 	}
 }
 
