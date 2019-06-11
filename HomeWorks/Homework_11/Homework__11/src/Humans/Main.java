86157f768174b97402b3e61821445d75ac054a20
Oleg Igonin <oleg_in_l@mail.ru>
2019-06-11 17:24:22 +0300

    remastered version application on Homework_11

--- HomeWorks/Homework_11/Homework__11/src/Humans/Main.java
+++ HomeWorks/Homework_11/Homework__11/src/Humans/Main.java
@@ -1,5 +1,6 @@
 package Humans;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,46 +8,42 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    Human[] humans = new Human[10];
+	    int maxValueAge = 0;
+	    int temp = 0;
+	    int k = 0;
 	    for (int i = 0; i < humans.length; i++) {
 	        humans[i] = new Human();
 	        System.out.println("Please, enter name a " + (i + 1) + " human");
             humans[i].name = scanner.next();
             System.out.println("Please, enter age a " + (i + 1) + " human");
             humans[i].age = scanner.nextInt();
+            if (humans[i].age > maxValueAge) {
+            	maxValueAge = humans[i].age;
+			}
         }
-	    int[] ages = new int[10];
-	    int temp = 0;
-	    int k = 0;
-	    int tempSlow = 0;
-	    for (int slowPointer = 0; slowPointer < humans.length; slowPointer++) {
-	    	for (int fastPointer = 1; fastPointer < humans.length; fastPointer++) {
-	    		if (humans[slowPointer].age == humans[fastPointer].age) {
-	    			temp++;
+	    int[] ages = new int[maxValueAge + 1];
+	    for (int i = 0; i < humans.length; i++) {
+	    	ages[humans[i].age]++;
+		}
+	    int[] result = new int[10];
+		System.out.println(Arrays.toString(ages));
+	    for (int i = 0; i < ages.length; i++) {
+	    	if (ages[i] > temp) {
+	    		k = 0;
+	    		for (int j = 0; j < result.length; j++) {
+	    			result[j] = 0;
 				}
+	    		result[k] = i;
+	    		temp = ages[i];
+			} else if (ages[i] == temp) {
+	    		k++;
+	    		result[k] = i;
 			}
-
-			if (temp > tempSlow) {
-				tempSlow = temp;
-				k = 0;
-				for (int i = 0; i < ages[i]; i++) {
-					ages[i] = 0;
-				}
-				ages[k] = humans[slowPointer].age;
-				k++;
-				temp = 0;
-			}
-			else if (temp == tempSlow) {
-				ages[k] = humans[slowPointer].age;
-				k++;
-				temp = 0;
-			}
-			temp = 0;
-
 		}
 		System.out.println("Common age: ");
-		for (int i = 0; i < ages.length; i++) {
-            if (ages[i] != 0) {
-                System.out.println(ages[i]);
+		for (int i = 0; i < result.length; i++) {
+            if (result[i] != 0) {
+                System.out.println(result[i]);
             }
         }
     }
